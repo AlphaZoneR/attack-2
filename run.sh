6c5d5924cdeb14f4ce3e01395a139cee00bbc045
ALBERT Ferencz <split@aferencz.xyz>
2022-01-20 17:34:55 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 for i in {15..25}
 do
-	mongosh --host "172.15.$i.3" -u user -p strongpassword11 < echo.js
+	echo $i
+	mongosh --host "172.$i.0.3" -u user -p strongpassword11 < echo.js
 done
 
 echo "Done"
+
+exit 0
