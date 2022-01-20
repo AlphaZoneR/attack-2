857e625b61a124d065ff1e0757bde084401d262a
ALBERT Ferencz <split@aferencz.xyz>
2022-01-20 17:50:19 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 {
-	mongosh --host "172.15.0.3" -u user -p strongpassword11 < echo.js &
-	mongosh --host "172.16.0.3" -u user -p strongpassword11 < echo.js &
-	mongosh --host "172.17.0.3" -u user -p strongpassword11 < echo.js &
-	mongosh --host "172.18.0.3" -u user -p strongpassword11 < echo.js &
-	mongosh --host "172.19.0.3" -u user -p strongpassword11 < echo.js &
-	mongosh --host "172.20.0.3" -u user -p strongpassword11 < echo.js
+	mongosh --host "172.15.0.2" -u user -p strongpassword11 < echo.js &
+	mongosh --host "172.16.0.2" -u user -p strongpassword11 < echo.js &
+	mongosh --host "172.17.0.2" -u user -p strongpassword11 < echo.js &
+	mongosh --host "172.18.0.2" -u user -p strongpassword11 < echo.js &
+	mongosh --host "172.19.0.2" -u user -p strongpassword11 < echo.js &
+	mongosh --host "172.20.0.2" -u user -p strongpassword11 < echo.js
 }
 
 echo "done"
